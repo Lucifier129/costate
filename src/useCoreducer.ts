9caf7662b01788e03660fc2e3eaa8d79393fee5d
yj_gu <yj_gu@ctrip.com>
2019-08-02 11:10:47 +0800

    fixed: useCoreducer not respond when costate changed outof dispatch

--- src/useCoreducer.ts
+++ src/useCoreducer.ts
@@ -1,5 +1,5 @@
 import React, { Dispatch } from 'react'
-import co, { read } from './costate'
+import co, { watch, read, hasCostate } from './costate'
 
 const { useState, useMemo, useRef, useCallback, useEffect } = React
 
@@ -13,6 +13,7 @@ export default function useCoreducer<State extends any[] | object, Action>(
   let dispatch = useCallback<Dispatch<Action>>(
     action => {
       coreducerRef.current(costate, action)
+      // update state directly
       setState(read(costate))
     },
     [costate]
@@ -21,6 +22,17 @@ export default function useCoreducer<State extends any[] | object, Action>(
   useEffect(() => {
     coreducerRef.current = coreducer
   }, [coreducer])
+
+  useEffect(() => {
+    /**
+     * if initialState is linked a costate, no need to watch it
+     * it must be watched by parent or another useCostate in function-component
+     */
+    if (hasCostate(initialState)) return
+    // setState will not cause re-render if the new state is equal the old one
+    // so it is safe to watch costate, re-render when costate is mutated out side the dispatch
+    return watch(costate, setState)
+  }, [])
 
   return [read(costate), dispatch]
 }
